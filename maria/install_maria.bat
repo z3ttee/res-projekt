d2968021566fcd4dd85ac6298dadb624e4cef24d
Zitzmann <cedric.zitzmann@gmail.com>
2021-11-07 00:03:14 +0100

    updated

--- maria/install_maria.bat
+++ maria/install_maria.bat
@@ -1,18 +1,41 @@
 @echo off
 chcp 65001
 
-set /p rootpass="Root-Passwort festlegen: "
-set /p setupNewDB="Neue Datenbank und Benutzer anlegen? [N für Nein, Beliebige Eingabe für Ja]: "
+set /p DUMMY="Um mit der Installation fortzufahren, drücke ENTER"
 
-if %setupNewDB%==N or if %setupNewDB%==n goto SETUPROOT
+echo 1) Zip wird entpackt
+tar -xf mariadb-10.6.4-winx64.zip
+set mariaDir=%~dp0mariadb-10.6.4-winx64
+echo Zip wurde in '%mariaDir%' entpackt
 
+echo.
+echo 2) MariaDB wird zu Umgebungsvariablen hinzugefügt
+set PATH=%PATH%;%mariaDir%\bin
+
+echo.
+echo 3) MariaDB wird gestartet (mysqld)
+start mysqld &
+
+echo.
+echo 4) Hauptbenutzer einrichten
+set /p rootpass="Passwort festlegen: "
+
+echo.
+echo 5) Passwort wird aktualisiert
+
+cmd /k mysql -u root --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY '%rootpass%'; FLUSH PRIVILEGES;"
+
+choice /c yn /m "Neue Datenbank und Benutzer anlegen? [Y, N] " /n
+
+if %ERRORLEVEL% EQU 1 goto SETUPDATABASE
+if %ERRORLEVEL% EQU 2 goto END
+
+:SETUPDATABASE
 set /p dbuser="Datenbank-Benutzername: "
 set /p dbpass="Passwort: "
 set /p dbname="Datenbank-Name: "
 
-:SETUPROOT
-ECHO Connecting to database and changing root password...&echo\
-cmd /k mysql -u root --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY '%rootpass%'; FLUSH PRIVILEGES;"
-ECHO Done.&echo\
+goto END
 
+:END
 PAUSE
