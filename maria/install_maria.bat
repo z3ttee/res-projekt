6745d93c22ed168beb71969642c1cbeb7a30211c
Zitzmann <cedric.zitzmann@gmail.com>
2021-11-10 16:30:15 +0100

    updated

--- maria/install_maria.bat
+++ maria/install_maria.bat
@@ -3,24 +3,71 @@ chcp 65001
 
 set /p DUMMY="Um mit der Installation fortzufahren, drücke ENTER"
 
+
+
 echo 1) Zip wird entpackt
+
 tar -xf mariadb-10.6.4-winx64.zip
 set mariaDir=%~dp0mariadb-10.6.4-winx64
 echo Zip wurde in '%mariaDir%' entpackt
 
-echo.
-echo 2) MariaDB wird zu Umgebungsvariablen hinzugefügt
-set PATH=%PATH%;%mariaDir%\bin
+
+
 
 echo.
-echo 3) MariaDB wird gestartet (mysqld)
-start mysqld &
+echo 2) MariaDB wird zu Umgebungsvariablen hinzugefügt
+
+REM Set home variable, so that mariadb knows where to look for files
+setx MARIADB_HOME "%mariaDir%"
+
+
+
+REM Tell windows where mariadb binaries are located, so that they can be executed (eg.: mysqld)
+setx var1 ^"%PATH%;^%MARIADB_BIN^%^" /m
+
+REM Set path variable in different env variable. This tells where the binaries are located.
+setx MARIADB_BIN "%mariaDir%\bin"
+
+
+echo.
+echo 3) MariaDB installieren >> mysql_install_db
+
+REM According to the docs, this is required to create all default tables needed for operation.
+mysql_install_db
+
+echo.
+
+
+
+
+
+
+echo.
+echo 4) MariaDB wird gestartet >> mysqld
+
+REM This starts mariadb in different cmd window. This allows for parallel work.
+start mysqld
+
+goto END
+
+
+
+
 
 echo.
 echo 4) Hauptbenutzer einrichten
 set /p rootpass="Passwort festlegen: "
 
 echo.
+
+
+
+
+
+
+
+
+
 echo 5) Passwort wird aktualisiert
 
 cmd /k mysql -u root --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY '%rootpass%'; FLUSH PRIVILEGES;"
