7f39a27037a3a2acf2a74ecd7b9b1ab417eb796c
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-11-14 18:33:23 +0100

    updated

--- api/src/app/ormconfig.ts
+++ api/src/app/ormconfig.ts
@@ -3,14 +3,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const config:TypeOrmModuleOptions =  {
 	type: "mariadb",
-	database: "guenther",
+	database: "guentherdb",
 	entities: [
 		"dist/**/*.entity{.ts,.js}"
 	 ],
 	synchronize: true, //only for Debug or it will completly wrap the Database on each change
 	host:"localhost",
 	port: 3306,
-	username: "root",
-	password: ""
+	username: "test",
+	password: "test"
 }
 
