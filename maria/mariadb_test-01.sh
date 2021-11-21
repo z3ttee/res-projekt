3bfa612b265d0fe3dc51a1b8db169f3ccce63556
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-11-21 19:40:33 +0100

    fixed

--- maria/mariadb_test-01.sh
+++ maria/mariadb_test-01.sh
@@ -31,7 +31,7 @@ echo "[1] Datenbank '$dbname' wird erstellt..."
 # Das SQL-Statement ist dafür verantwortlich, dass eine neue Datenbank angelegt wird.
 # Zum SQL-Statement:
 # CREATE DATABASE = Erstellt eine neue Tabelle mit dem nachfolgenden Parameter als Name.
-mysql --user=root --password="$rootpw" --execute="CREATE DATABASE $dbname;"
+mysql --user=root --password=$rootpw --execute="CREATE DATABASE $dbname;"
 
 # Der Nutzer wird über den derzeitigen Schritt informiert.
 echo "[2] Benutzer '$dbuser' wird erstellt..."
@@ -48,7 +48,7 @@ echo "[2] Benutzer '$dbuser' wird erstellt..."
 # Zum SQL-Statement:
 # CREATE USER = Erstellt einen neuen Nutzer mit nachfolgendem Parameter als Nutzername bzw. Identifikation. (@ bestimmt den Hostnamenkontext, % bedeutet alle Hosts)
 # IDENTIFIED BY = Legt das Passwort für den neuen Benutzer fest.
-mysql --user=root --password="$rootpw" --execute="CREATE USER '$dbuser'@'%' IDENTIFIED BY '$dbpass';"
+mysql --user=root --password=$rootpw --execute="CREATE USER '$dbuser'@'%' IDENTIFIED BY '$dbpass';"
 
 # Der Nutzer wird über den derzeitigen Schritt informiert.
 echo "[3] Rechte werden auf Datenbank angewendet..."
@@ -67,7 +67,7 @@ echo "[3] Rechte werden auf Datenbank angewendet..."
 # ON = Bestimmt den Datenbankkontext. Betrifft das alle Tabellen in einer DB, gibt man .* nach dem DB-Name an. (Sollte die Berechtigung nur für eine bestimmte Tabelle in der DB geändert werden, wäre die Syntax <dbname>.<tablename>)
 # TO = Bestimmt den betroffenen Nutzer dieser Änderungen.
 # FLUSH PRIVILEGES = Lädt Berechtigungen neu und lässt diese somit in Kraft treten
-mysql --user=root --password="$rootpw" --execute="GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'%'; FLUSH PRIVILEGES;"
+mysql --user=root --password=$rootpw --execute="GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'%'; FLUSH PRIVILEGES;"
 
 # Der Nutzer wird über den derzeitigen Schritt informiert.
 echo "[4] Fertig."
