6b36616cd3c2674298c8dc15867a5109f3cc5aa0
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-11-21 18:55:37 +0100

    Update mariadb_test-01.sh

--- maria/mariadb_test-01.sh
+++ maria/mariadb_test-01.sh
@@ -5,12 +5,18 @@ echo "MariaDB-Server === Test 01 === Datenbank + Benutzer erstellen"
 
 # Leere Zeile ausgeben
 echo
+# Der Nutzer wird zu einer Eingabe des Root-passworts (der Datenbank) aufgefordert, die in der Variable "rootpw" gespeichert wird.
+read -s -p "DB Root-Passwort: " rootpw
+echo
 # Der Nutzer wird zu einer Eingabe aufgefordert, die in der Variable "dbname" gespeichert wird.
 read -p "Datenbankname festlegen: " dbname
+echo
 # Der Nutzer wird zu einer Eingabe aufgefordert, die in der Variable "dbuser" gespeichert wird.
 read -p "Benutzername festlegen: " dbuser
+echo
 # Der Nutzer wird zu einer Eingabe aufgefordert, die in der Variable "dbpass" gespeichert wird.
 read -p "Passwort festlegen: " dbpass
+echo
 
 # Der Nutzer wird über den derzeitigen Schritt informiert.
 echo "[1] Datenbank '$dbname' wird erstellt..."
@@ -25,7 +31,7 @@ echo "[1] Datenbank '$dbname' wird erstellt..."
 # Das SQL-Statement ist dafür verantwortlich, dass eine neue Datenbank angelegt wird.
 # Zum SQL-Statement:
 # CREATE DATABASE = Erstellt eine neue Tabelle mit dem nachfolgenden Parameter als Name.
-sudo mysql --execute="CREATE DATABASE $dbname;"
+mysql --user=root --password="$rootpw" --execute="CREATE DATABASE $dbname;"
 
 # Der Nutzer wird über den derzeitigen Schritt informiert.
 echo "[2] Benutzer '$dbuser' wird erstellt..."
@@ -42,7 +48,7 @@ echo "[2] Benutzer '$dbuser' wird erstellt..."
 # Zum SQL-Statement:
 # CREATE USER = Erstellt einen neuen Nutzer mit nachfolgendem Parameter als Nutzername bzw. Identifikation. (@ bestimmt den Hostnamenkontext, % bedeutet alle Hosts)
 # IDENTIFIED BY = Legt das Passwort für den neuen Benutzer fest.
-sudo mysql --execute="CREATE USER '$dbuser'@'%' IDENTIFIED BY '$dbpass';"
+mysql --user=root --password="$rootpw" --execute="CREATE USER '$dbuser'@'%' IDENTIFIED BY '$dbpass';"
 
 # Der Nutzer wird über den derzeitigen Schritt informiert.
 echo "[3] Rechte werden auf Datenbank angewendet..."
@@ -61,7 +67,7 @@ echo "[3] Rechte werden auf Datenbank angewendet..."
 # ON = Bestimmt den Datenbankkontext. Betrifft das alle Tabellen in einer DB, gibt man .* nach dem DB-Name an. (Sollte die Berechtigung nur für eine bestimmte Tabelle in der DB geändert werden, wäre die Syntax <dbname>.<tablename>)
 # TO = Bestimmt den betroffenen Nutzer dieser Änderungen.
 # FLUSH PRIVILEGES = Lädt Berechtigungen neu und lässt diese somit in Kraft treten
-sudo mysql --execute="GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'%'; FLUSH PRIVILEGES;"
+mysql --user=root --password="$rootpw" --execute="GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'%'; FLUSH PRIVILEGES;"
 
 # Der Nutzer wird über den derzeitigen Schritt informiert.
 echo "[4] Fertig."
